c62a00ab5dc1a53824e9edfde36d2f606f7ba982
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-07-23 17:29:32 -0700

    Add trivial jackson-jr/stree test

---
+++ src/test/java/com/fasterxml/jackson/integtest/jacksonjr/JacksonJrTreeTest.java
@@ -0,0 +1,43 @@
+package com.fasterxml.jackson.integtest.jacksonjr;
+
+import com.fasterxml.jackson.core.TreeNode;
+
+import com.fasterxml.jackson.integtest.BaseTest;
+
+import com.fasterxml.jackson.jr.ob.JSON;
+import com.fasterxml.jackson.jr.stree.JrSimpleTreeExtension;
+import com.fasterxml.jackson.jr.stree.JrsArray;
+import com.fasterxml.jackson.jr.stree.JrsObject;
+
+// Copied from `ReadViaJSONTest` of jackson-jr/jr-stree
+public class JacksonJrTreeTest extends BaseTest
+{
+    // 23-Jul-2020, tatu: Use the new mechanism for registering
+    private final JSON JSON_WITH_TREE = JSON.builder()
+            .register(new JrSimpleTreeExtension())
+        .build();
+
+    public void testSimpleList() throws Exception
+    {
+        final String INPUT = "[true,\"abc\"]";
+        // and then through jr-objects:
+        TreeNode node = JSON_WITH_TREE.treeFrom(INPUT);
+
+        assertTrue(node instanceof JrsArray);
+        assertEquals(2, node.size());
+
+        // actually, verify with write...
+        assertEquals(INPUT, JSON_WITH_TREE.asString(node));
+    }
+
+    public void testSimpleMap() throws Exception
+    {
+        final String INPUT = "{\"a\":1,\"b\":true,\"c\":3}";
+        TreeNode node = JSON_WITH_TREE.treeFrom(INPUT);
+        assertTrue(node instanceof JrsObject);
+        assertEquals(3, node.size());
+
+        assertEquals(INPUT, JSON_WITH_TREE.asString(node));
+    }
+
+}
