3c37745c3b4b6e68ab989e2c5ab802845047875b
henarons <hba@finn.no>
2020-12-11 11:13:38 +0100

    Add kotlin/xml related test that breaks Jackson 2.12.0
    See https://github.com/FasterXML/jackson-module-kotlin/issues/396

---
+++ src/test/kotlin/com/fasterxml/jackson/integtest/kotlin/Jackson212MissingConstructorTest.kt
@@ -0,0 +1,27 @@
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule
+import com.fasterxml.jackson.dataformat.xml.XmlFactory
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.integtest.BaseTest
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import javax.xml.stream.XMLInputFactory
+
+class Jackson212MissingConstructorTest : BaseTest()
+{
+    /**
+     * Succeeds in Jackson 2.11.x, but fails in Jackson 2.12.0
+     * See https://github.com/FasterXML/jackson-module-kotlin/issues/396
+     */
+    fun testMissingConstructor()
+    {
+        val factory = XmlFactory(XMLInputFactory.newInstance())
+        val mapper = XmlMapper(factory, JacksonXmlModule()).registerKotlinModule()
+
+        val xml = "<product><stuff></stuff></product>"
+        val product: Product = mapper.readValue(xml, Product::class.java)
+
+        assertEquals(Product(null), product)
+    }
+
+    private data class Stuff(val str: String?)
+    private data class Product(val stuff: Stuff?)
+}
