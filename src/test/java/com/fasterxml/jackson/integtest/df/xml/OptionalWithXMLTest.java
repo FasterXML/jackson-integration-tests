33a10411716f2de1fb8475dea84c657d1ea5e8f5
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-10-30 20:09:12 -0700

    Add passing test for https://github.com/FasterXML/jackson-modules-java8/issues/280

---
+++ src/test/java/com/fasterxml/jackson/integtest/df/xml/OptionalWithXMLTest.java
@@ -0,0 +1,47 @@
+package com.fasterxml.jackson.integtest.df.xml;
+
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.integtest.BaseTest;
+
+public class OptionalWithXMLTest extends BaseTest
+{
+    // For [modules-java8#280]
+    static class IntWrapper {
+        public int a;
+
+        public IntWrapper() {
+            a = 1;
+        }
+    }
+
+    static class OptionalBean {
+        public Optional<IntWrapper> opt = Optional.empty();
+    }
+    
+    private final ObjectMapper MAPPER = xmlMapperBuilder()
+            .enable(ToXmlGenerator.Feature.WRITE_NULLS_AS_XSI_NIL)
+            .addModule(new Jdk8Module())
+            .build();
+
+    // For [modules-java8#280]: working case when enabling `xsi:nil` writes:
+    public void testOptionalWithXMLWriteAndRead() throws Exception
+    {
+        String doc = MAPPER.writeValueAsString(new OptionalBean());
+                 
+        OptionalBean result = MAPPER.readValue(doc, OptionalBean.class);
+        assertEquals(Optional.empty(), result.opt);
+    }
+
+    // For [modules-java8#280]: working case with `xsi:nil` as input
+    public void testOptionalWithXMLReadWithNil() throws Exception
+    {
+        final String doc = "<OptionalBean><opt xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:nil='true'/></OptionalBean>";
+                 
+        OptionalBean result = MAPPER.readValue(doc, OptionalBean.class);
+        assertEquals(Optional.empty(), result.opt);
+    }
+}
