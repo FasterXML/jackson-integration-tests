29f784644e9199c530b272de6589d853e5cc040d
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-03 15:30:59 -0800

    More module work

---
+++ src/test/java/module-info.java
@@ -0,0 +1,34 @@
+// Module-info for integration tests
+module tools.jackson.integtest
+{
+    // Core deps
+    requires com.fasterxml.jackson.annotation;
+    requires tools.jackson.core;
+    requires tools.jackson.databind;
+
+    // Data formats
+    requires tools.jackson.dataformat.avro;
+    requires tools.jackson.dataformat.cbor;
+    requires tools.jackson.dataformat.csv;
+    requires tools.jackson.dataformat.protobuf;
+    requires tools.jackson.dataformat.properties;
+    requires tools.jackson.dataformat.smile;
+    requires tools.jackson.dataformat.xml;
+    requires tools.jackson.dataformat.yaml;
+
+    // Base modules
+
+    // Data types 
+    requires tools.jackson.datatype.joda;
+    requires tools.jackson.datatype.javatime;
+    requires tools.jackson.datatype.jsonp;
+    requires tools.jackson.datatype.jsr353;
+    requires tools.jackson.datatype.guava;
+    
+    // Other Jackson components
+    requires tools.jackson.jr.ob;
+
+    // 3rd party
+    requires com.google.common;
+    requires org.joda.time;
+}
