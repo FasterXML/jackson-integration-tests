a9ecd8b293a0bda04de92ba5127ed2ccb0abbb04
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-04 16:10:03 -0800

    Ion, Properties test improvements

--- src/test/java/tools/jackson/integtest/df/basic/BasicReadWriteIonTest.java
+++ src/test/java/tools/jackson/integtest/df/basic/BasicReadWriteIonTest.java
@@ -2,20 +2,29 @@ package tools.jackson.integtest.df.basic;
 
 import org.junit.jupiter.api.Test;
 
-import tools.jackson.databind.ObjectMapper;
-
+import tools.jackson.dataformat.ion.IonFactory;
+import tools.jackson.dataformat.ion.IonObjectMapper;
 import tools.jackson.integtest.BaseTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class BasicReadWriteIonTest extends BaseTest
 {
     @Test
     public void testSimple() throws Exception
     {
-        ObjectMapper mapper = ionMapper();
+        IonObjectMapper mapper = ionMapperBuilder(IonFactory.forBinaryWriters()).build();
 
-        // !!! 23-Feb-2016, tatu: Trivial to avoid using any Schema
-        assertNotNull(mapper);
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("key", Integer.valueOf(1972));
+        input.put("key2", true);
+        input.put("key3", "abc");
+        byte[] encoded = mapper.writeValueAsBytes(input);
+
+        Map<?,?> output = mapper.readValue(encoded, Map.class);
+        assertEquals(input, output);
     }
 }
--- src/test/java/tools/jackson/integtest/df/basic/BasicReadWritePropertiesTest.java
+++ src/test/java/tools/jackson/integtest/df/basic/BasicReadWritePropertiesTest.java
@@ -1,25 +1,43 @@
 package tools.jackson.integtest.df.basic;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
 import tools.jackson.databind.ObjectMapper;
 
 import tools.jackson.integtest.BaseTest;
+import tools.jackson.integtest.BaseTest.PointXYZ;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicReadWritePropertiesTest extends BaseTest
 {
-    @Test
-    public void testSimple() throws Exception
-    {
-        ObjectMapper mapper = propsMapper();
-        final Map<String, Object> input = Collections.singletonMap("key", "value");
-        String doc = mapper.writeValueAsString(input);
+    private final ObjectMapper MAPPER = propsMapper();
 
-        Map<?,?> output = mapper.readValue(doc, Map.class);
+    @Test
+    public void testUntypedReadWrite() throws Exception
+    {
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("key", Integer.valueOf(1972));
+        input.put("key2", true);
+        input.put("key3", "abc");
+        String doc = MAPPER.writeValueAsString(input);
+
+        Map<?,?> output = MAPPER.readValue(doc, Map.class);
+        // NOTE: with Properties cannot retain type so it's all Strings now
+        Map<String, Object> exp = input.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().toString()));
+        assertEquals(exp, output);
+    }
+
+    @Test
+    public void testTypedReadWrite() throws Exception
+    {
+        PointXYZ input = new PointXYZ(1, 2, 3);
+        String doc = MAPPER.writeValueAsString(input);
+        PointXYZ output = MAPPER.readerFor(PointXYZ.class).readValue(doc);
         assertEquals(input, output);
     }
 }
