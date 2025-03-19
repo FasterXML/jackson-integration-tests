84902bcc1a9c3ffd73a0ad9666fbb333c9adfdeb
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-03-19 10:50:52 -0700

    Fix module names wrt 3.0 (com.fasterxml.jackson -> tools.jackson)

--- src/test/resources/tools/jackson/integtest/gradle/build.gradle.kts
+++ src/test/resources/tools/jackson/integtest/gradle/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 val modulesWithoutGradleMetadata = listOf(
-        "com.fasterxml.jackson.jr:jackson-jr-all", // TODO is there a reason not to add this?
+        "tools.jackson.jr:jackson-jr-all", // TODO is there a reason not to add this?
 
-        "com.fasterxml.jackson:jackson-bom", // does not need it
-        "com.fasterxml.jackson.module:jackson-module-scala_2.11", // built with sbt
-        "com.fasterxml.jackson.module:jackson-module-scala_2.12", // built with sbt
-        "com.fasterxml.jackson.module:jackson-module-scala_2.13", // built with sbt
-        "com.fasterxml.jackson.module:jackson-module-scala_3",     // built with sbt
+        "tools.jackson:jackson-bom", // does not need it
+        "tools.jackson.module:jackson-module-scala_2.11", // built with sbt
+        "tools.jackson.module:jackson-module-scala_2.12", // built with sbt
+        "tools.jackson.module:jackson-module-scala_2.13", // built with sbt
+        "tools.jackson.module:jackson-module-scala_3",     // built with sbt
 
         // 3rd-party managed dependencies, by XML module:
         "com.fasterxml.woodstox:woodstox-core",
@@ -28,7 +28,7 @@ repositories.mavenCentral()
 
 // (miss-)use a component metadata rule to collect all entries from the BOM
 val allJacksonModule = mutableListOf<String>()
-dependencies.components.withModule("com.fasterxml.jackson:jackson-bom") {
+dependencies.components.withModule("tools.jackson:jackson-bom") {
     allVariants {
         withDependencyConstraints {
             allJacksonModule.addAll(map { it.toString() })
@@ -108,5 +108,5 @@ tasks.register("checkMetadata") {
 
 fun resolveJacksonModules(allModules: Configuration) =
     allModules.incoming.resolutionResult.allComponents.filter {
-        it.moduleVersion!!.group.startsWith("com.fasterxml.jackson") && !modulesWithoutGradleMetadata.contains(it.moduleVersion!!.module.toString())
+        it.moduleVersion!!.group.startsWith("tools.jackson") && !modulesWithoutGradleMetadata.contains(it.moduleVersion!!.module.toString())
     }
