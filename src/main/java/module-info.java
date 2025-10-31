0df3a94fed343ae82a410de85a02799c5d2ed99c
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-10-31 16:51:14 -0700

    Fix module dep for mr-bean module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -41,7 +41,7 @@ module tools.jackson.integtest.base
     requires transitive tools.jackson.module.kotlin;
 
     // 31-Oct-2025, tatu: Broken module def due to shading in 3.0?
-    // requires transitive tools.jackson.module.mrbean;
+    requires transitive tools.jackson.module.mrbean;
 
     // 3rd party
     requires transitive com.google.common;
