1839132c5b1cef45c88b5383db2c78d217ae3559
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-11-13 09:55:19 -0800

    Fix 3.0 vs 3.1 Gradle dep problem

--- src/test/resources/tools/jackson/integtest/gradle/build.gradle.kts
+++ src/test/resources/tools/jackson/integtest/gradle/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
     //                                                  | 
     //                                                  V
 
-    implementation(platform("tools.jackson:jackson-bom:+"))
-    //implementation(platform("tools.jackson:jackson-bom:3.0.1-SNAPSHOT"))
+    implementation(platform("tools.jackson:jackson-bom:3.0.2"))
+    //implementation(platform("tools.jackson:jackson-bom:3.0.3-SNAPSHOT"))
     // Need just new Snapshot repo (everything)
     repositories.maven("https://central.sonatype.com/repository/maven-snapshots");
 }
