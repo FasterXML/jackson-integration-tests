0f31ffb5faf5265e83bf907dc069e0e09e4f75c7
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-11-26 18:38:15 +0100

    Gradle test: configure cacheChangingModulesFor for detached configurations (#36)

--- src/test/resources/com/fasterxml/jackson/integtest/gradle/build.gradle.kts
+++ src/test/resources/com/fasterxml/jackson/integtest/gradle/build.gradle.kts
@@ -55,7 +55,7 @@ tasks.register("checkMetadata") {
         configurations.compileClasspath.get().resolve() // triggers the rule above
 
         // Create dependencies to all Modules references in the BOM
-        val allModules = configurations.detachedConfiguration(*allJacksonModule.map { dependencies.create(it) }.toTypedArray())
+        val allModules = resolver(allJacksonModule)
         val modulesWithGradleMetadata = allJacksonModule.filter { m -> modulesWithoutGradleMetadata.none { m.startsWith(it) } }
 
         // Tell Gradle to do the dependency resolution and return the result with dependency information
@@ -78,7 +78,7 @@ tasks.register("checkMetadata") {
                 ignoreGradleMetadataRedirection()
             }
         }
-        val pomAllModules = configurations.detachedConfiguration(*allJacksonModule.map { dependencies.create(it) }.toTypedArray())
+        val pomAllModules = resolver(allJacksonModule)
         val pomAllModulesResolved = resolveJacksonModules(pomAllModules)
         allModulesResolved.forEachIndexed { index, gmmModule ->
             val pomModule = pomAllModulesResolved[index]
@@ -93,9 +93,9 @@ tasks.register("checkMetadata") {
             }
         }
 
-        val pomMetadataFiles = configurations.detachedConfiguration(*modulesWithGradleMetadata.map { dependencies.create("$it@pom") }.toTypedArray())
-        val gradleMetadataFiles = configurations.detachedConfiguration(*modulesWithGradleMetadata.map { dependencies.create("$it@module") }.toTypedArray())
-        val checksumFiles = configurations.detachedConfiguration(*modulesWithGradleMetadata.map { dependencies.create("$it@jar.md5") }.toTypedArray())
+        val pomMetadataFiles = resolver(modulesWithGradleMetadata.map { "$it@pom" })
+        val gradleMetadataFiles = resolver(modulesWithGradleMetadata.map { "$it@module" })
+        val checksumFiles = resolver(modulesWithGradleMetadata.map { "$it@jar.md5" })
 
         val pomsWithoutMarker = pomMetadataFiles.files.filter { !it.readText().contains("<!-- do_not_remove: published-with-gradle-metadata -->") }.map { it.name }
         if (pomsWithoutMarker.isNotEmpty()) {
@@ -122,3 +122,7 @@ fun resolveJacksonModules(allModules: Configuration) =
     allModules.incoming.resolutionResult.allComponents.filter {
         it.moduleVersion!!.group.startsWith("com.fasterxml.jackson") && !modulesWithoutGradleMetadata.contains(it.moduleVersion!!.module.toString())
     }
+
+fun resolver(deps: List<String>) = configurations.detachedConfiguration(
+    *deps.map { dependencies.create(it) }.toTypedArray()).also {
+        it.resolutionStrategy.cacheChangingModulesFor(0, "seconds") }
